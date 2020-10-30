bec90e6dc19d73f339ce072dcbeedd76c10ec4d3
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2020-10-30 09:15:17 +0530

    Created and documented server.js with successful server running based on async CORS config and a basic test GET request route

---
+++ Angular_8_JWT_Authentication_Authorization/express_mysql_jwt_authentication/server.js
@@ -0,0 +1,65 @@
+/**
+ * This is server.js, which is the main module of the back-end application
+ * Importing the very first required dependencies for the application
+ * (Referring the Express.js documentation), the CORS is configured asynchronously
+ */
+
+ /* 'express' is imported as the core framework library module, used to build the RES api */
+const express = require('express');
+
+/* 'body-parser' is imported to parse the request and build the request.body object */
+const bodyParser = require('body-parser');
+
+/* 'cors' is imported to configure the CORS policies for successful communication between the front-end and the back-end */
+const cors = require('cors');
+
+
+
+                        /////   -   ***   -   Done importing till here in application   -   ***   -   /////
+
+
+
+/* Creating Server for the back-end application */
+var app = express();
+
+/* List of the allowed hosts */
+const allowList = ['http://localhost:4200'];
+
+/* CORS configuration and origin validation */
+const corsOptionsDelegate = (request, callback) => {
+                                let corsOptions = null;
+
+                                if (allowList.indexOf(request.header('Origin')) !== -1)
+                                    corsOptions = {origin: true}    /* Reflect (enable) the requested origin in the CORS response */
+                                else
+                                    corsOptions = {origin: false}   /* Disable CORS for this request */
+
+                                /* callback() expects two arguments (error, options)
+                                * Here, error = null and options = corsOptions
+                                */
+                                callback(null, corsOptions);
+                            }
+
+/* Parse request of content-type: application/json */
+app.use(bodyParser.json());
+
+/* Parse request of content-type: application/x-www-form-urlencoded */
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/* Any HTTP method called on application server object has the argument sequence as follows --
+ * 1. Request End-point (URI)
+ * 2. CORS configuration and origin validation
+ * 3. Handler function
+ */
+app.get('/', cors(corsOptionsDelegate), 
+            (request, response) => {
+                response.json({ message: 'Hey, this is great, a Express server using Node.js after long time! are the changes loading?? :-/' })
+            });
+
+
+/* Set port and listen for request over it */
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+                    console.log(`\nServer is listening on port ${PORT}!`);
+                });
