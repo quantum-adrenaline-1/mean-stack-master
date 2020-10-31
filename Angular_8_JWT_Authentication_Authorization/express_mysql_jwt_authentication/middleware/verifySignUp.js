006b0bf1933b08c3e36e9404cca46c7aeb9cb032
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2020-10-31 20:06:02 +0530

    Created verifySignUp.js and implemented logic to check up for username and email duplication and specified roles existence

---
+++ Angular_8_JWT_Authentication_Authorization/express_mysql_jwt_authentication/middleware/verifySignUp.js
@@ -0,0 +1,60 @@
+const db = require('../models');
+const ROLES = db.ROLES;
+const User = db.user;
+
+const checkDuplicateUsernameOrEmail = (request, response, next) => {
+                                    // Username duplication check
+                                    User.findOne({
+                                        where: {
+                                            username: request.body.username
+                                        }
+                                    }).then(user => {
+                                        if (user) {
+                                            response.status(400).send({
+                                                message: "Failed! Username is already in use!"
+                                            });
+                                            return;
+                                        }
+
+                                        // Email duplication check
+                                        User.findOne({
+                                            where: {
+                                                email: request.body.email
+                                            }
+                                        }).then(user => {
+                                            if (user) {
+                                                response.status(400).send({
+                                                    message: "Failed! EmailId already exists!"
+                                                });
+                                                return;
+                                            }
+
+                                            next();
+                                        });
+                                    });
+                                };
+
+
+const checkRolesExisted = (request, response, next) => {
+                            const requestRoles = request.body.roles;
+                            if (requestRoles) {
+                                for (let i = 0; i < requestRoles.length; i++) {
+                                    if (! ROLES.includes(requestRoles[i])) {
+                                        response.status(400).send({
+                                            message: "Failed! Role does not exist -- " + requestRoles[i]
+                                        });
+                                        return;
+                                    }
+                                }
+                            }
+
+                            next();
+                        };
+
+
+const verifySignUp = () => {
+                        checkDuplicateUsernameOrEmail,
+                        checkRolesExisted                 
+                    };
+
+module.exports = verifySignUp;
