0e72b40969f9692f74dc79b2b7fbf183ed942a70
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-07 13:58:45 +0530

    Implemented test and access routing functions for all the roles of user in user.routes.js

--- Angular_8_JWT_Authentication_Authorization/express_mysql_jwt_authentication/routes/user.routes.js
+++ Angular_8_JWT_Authentication_Authorization/express_mysql_jwt_authentication/routes/user.routes.js
@@ -1,1 +1,38 @@
-const { authJwt } = require("../middleware")
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/user.controller");
+const { request, response } = require("express");
+
+module.exports = app => {
+                    app.use((request, response, next) => {
+                        response.header(
+                            "Access-Controll-Allow-Headers",
+                            "x-access-token, Content-Type, Accept, Origin"
+                        );
+
+                        next();
+                    });
+
+                    /**
+                     * Test route which is available all time to access
+                     * Most likely, use to check for server status if it is responding to the user requests or not
+                     */
+                    app.get("/api/test/all", controller.allAccess);
+
+                    /**
+                     * This is a priviledge access route for 'User' role
+                     * A valid access token required
+                     */
+                    app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+
+                    /**
+                     * This is a priviledge access route for 'Moderator' role
+                     * A valid access token required
+                     */
+                    app.get("/api/test/mod", [authJwt.verifyToken, authJwt.isModerator], controller.moderatorBoard);
+
+                    /**
+                     * This is a priviledge access route for 'Admin' role
+                     * A valid access token required
+                     */
+                    app.get("/api/test/admin", [authJwt.verifyToken, authJwt.isAdmin], controller.adminBoard);
+                };
