f995e255b7c4ddaef5b20174b9d4726f5d86a889
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-07 11:48:32 +0530

    Configured request and response for testing authorization in user.controller.js

---
+++ Angular_8_JWT_Authentication_Authorization/express_mysql_jwt_authentication/controllers/user.controller.js
@@ -0,0 +1,17 @@
+const { request, response } = require("express");
+
+exports.allAccess = (request, response) => {
+                        response.status(200).send("Public Content");
+                    };
+
+exports.userBoard = (request, response) => {
+                        response.status(200).send("User Content");                    
+                    };
+
+exports.adminBoard = (request, response) => {
+                        response.status(200).send("Admin Content");
+                    };
+
+exports.moderatorBoard = (request, response) => {
+                            response.status(200).send("Moderator Content");
+                        };
