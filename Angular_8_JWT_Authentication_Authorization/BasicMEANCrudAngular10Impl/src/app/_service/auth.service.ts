4efbcb03d5149b7a27ab932a4eb456a387cf0ae6
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-07 22:24:37 +0530

    Implemented and documented the Authentication handling service in auth.service.ts for BasicMEANCrudAngular10Impl

--- Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/_service/auth.service.ts
+++ Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/_service/auth.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+/* Backend endpoint url for Authentication and Authorization */
+const AUTH_API = "http://localhost:8080/api/auth";
+
+/* Creating request header for setting 'Content Type' for Authentication service */
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  /* Injecting HttpClient as dependency for serving as Http Client Request Tool Operator for Authentication Service */
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  /**
+   * This is login service handler function
+   * It required user credentials to add in the request body
+   * 
+   * The response of this function is generic implementation of 'Observable' type 'any'
+   * This makes the request and response handling reactive. The application execution will observer/monitor for the response over this route
+   * and process it as soon as it is obtained
+   * 
+   * @param credentials 
+   */
+  login(credentials): Observable<any> {
+    // The request is constructed with url, body (requires username and password for login) and header before dispatch
+    return this.httpClient.post(`${AUTH_API}/signin`, {
+      username: credentials.username,
+      password: credentials.password
+    }, httpOptions);
+  }
+
+  register(user): Observable<any> {
+    // The request is constructed with url, body (requires username, password and email for registration) and header before dispatch
+    return this.httpClient.post(`${AUTH_API}/signup`, {
+      username: user.username,
+      password: user.password,
+      email: user.email
+    }, httpOptions);
+  }
 }
