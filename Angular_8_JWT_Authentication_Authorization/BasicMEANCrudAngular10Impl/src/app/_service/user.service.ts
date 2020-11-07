dbd49e4b00111da320d1ba10e608c5cddc1be58b
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-07 23:15:01 +0530

    Implemented service for handling the role validation endpoints

--- Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/_service/user.service.ts
+++ Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/_service/user.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+/* Backend endpoint url for Authentication and Authorization */
+const API_URL = 'http://localhost:8080/api/test';
+
+/**
+ * This class is used to handle the test endpoints to confirm the user role
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  /* Injecting HttpClient as dependency for serving as Http Client Request Tool Operator for Authentication Service */
+  constructor(private http: HttpClient) { }
+
+  getPublicContent(): Observable<any> {
+    return this.http.get(`${API_URL}/all`, { responseType: 'text' });
+  }
+
+  getUserBoard(): Observable<any> {
+    return this.http.get(`${API_URL}/user`, { responseType: 'text' });
+  }
+
+  getModeratorBoard(): Observable<any> {
+    return this.http.get(`${API_URL}/mod`, { responseType: 'text' });
+  }
+
+  getAdminBoard(): Observable<any> {
+    return this.http.get(`${API_URL}/admin`, { responseType: 'text' });
+  }
 }
