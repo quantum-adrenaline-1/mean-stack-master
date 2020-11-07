331e5d5b8854e674310206a71c16289f2cbdbdc3
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-07 13:30:57 +0530

    Moved the misplaced package 'routes' out of 'config' package to the application root directory

---
+++ Angular_8_JWT_Authentication_Authorization/express_mysql_jwt_authentication/routes/auth.routes.js
@@ -0,0 +1,34 @@
+const {verifySignUp} = require('../middleware');
+const controller = require("../controller/auth.controller");
+const { request, response } = require('express');
+
+module.exports = app => {
+                    /** 
+                     * Headers are constructed before sending HTTP response for authentication (signup and signin)
+                     * After the Header are constructed, any of the requested authentication route is accessed using the next() function
+                     */
+                    app.use((request, response, next) => {
+                        response.header(
+                            "Access-Controll-Allow-Headers",
+                            "x-access-token, Origin, Content-Type, Accept"
+                        );
+                        next();
+                    });
+
+                    /**  
+                     * HTTP POST request for 'signup'
+                     * The functions with business logic for 'sign up' are executed for this route 
+                     */
+                    app.post(
+                        "/api/auth/signup",
+                        [
+                            verifySignUp.checkDuplicateUsernameOrEmail,
+                            verifySignUp.checkRolesExisted
+                        ]
+                    );
+
+                    /**
+                     * The controller function with business logic for 'login' is executed for this route 
+                     */
+                    app.post("/api/auth/signin", controller.signin)
+                }
---
+++ Angular_8_JWT_Authentication_Authorization/express_mysql_jwt_authentication/routes/user.routes.js
@@ -0,0 +1,1 @@
+const { authJwt } = require("../middleware")
