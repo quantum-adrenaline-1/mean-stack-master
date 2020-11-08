9b321b4336a46133b6505e799a7dfef06cc4f734
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-08 14:57:06 +0530

    Implemented the frontend logic to handle registration in register.component.ts for BasicMEANCrudAngular10Impl

--- Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/register/register.component.ts
+++ Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../_service/auth.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // fundamental and most important field of the 'register' component class used to fill in the user details submitted during registration
+  form: any = { };
+  
+  // Two flags below used to monitor and validate the status of registration / 'signup'
+  isSuccessful = false;
+  isSignUpFailed = false;
 
+  // field of the 'register' component class to fill in error message on error occurance
+  errorMessage = '';
+
+  /* Authentication Service injected as dependency for valid registration as per the business logic */
+  constructor(private authService: AuthService) { }
+
+  /* Method of the OnInit interface to implement */
   ngOnInit(): void {
+  }
+
+  /* Method handling the 'submit' action for 'registration' form in 'register' (this) component */
+  onSubmit(): void {
+    this.authService.register(this.form)
+      .subscribe(
+        successResponse => {
+        console.log(JSON.stringify(successResponse));
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      }, error => {
+        this.errorMessage = error.error.message;
+        this.isSignUpFailed = true;
+        this.isSuccessful = false;
+      }
+    );
   }
 
 }
