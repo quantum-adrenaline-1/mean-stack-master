af61f0e6801872cd5faccc91998b6ec4d143dd25
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-09 21:32:11 +0530

    Implemented the component class for handling login inside 'login' component in BasicMEANCrudAngular10Impl angular frontend

--- Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/login/login.component.ts
+++ Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/login/login.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_service/auth.service';
+import { TokenStorageService } from '../_service/token-storage.service';
 
+/**
+ * The component class for login form invovles fields such as 'username' and 'password'
+ * 'onSubmit()' method is used to handle the submit action of form
+ * There are flag field like 'isLoggedIn' and 'isLoginFailed' for implementing validations and error UI acknowledgement handling in the template
+ * The form object possess the submitted credentials for login
+ */
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +15,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: any = { };
+  isLoggedIn: boolean = false;
+  isLoginFailed: boolean = false;
+  errorMessage: string = '';
+  roles: string[] = [];
+
+  constructor(
+    private authService: AuthService,
+    private tokenStorageService: TokenStorageService) { }
 
   ngOnInit(): void {
+    if (this.tokenStorageService.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorageService.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    this.authService.login(this.form).subscribe(
+      successResponse => {
+        /* Executing the service functions first to process complete of the response data */
+        this.tokenStorageService.saveToken(successResponse.accessToken);
+        this.tokenStorageService.saveUser(successResponse);
+
+        /* The field variables are now set to build state of 'login' component as per the response data */
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorageService.getUser().roles;
+
+        /* Page is reloaded to update the changes */
+        this.pageReload();
+      }
+    )
+  }
+
+  pageReload(): void {
+    window.location.reload();
   }
 
 }
