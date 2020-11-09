a75e22dd3e5ffd6ae580b3a71004fbe88e21714c
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-09 22:26:33 +0530

    Implemented component class for 'home' component to load public content from backend application in BasicMEANCrudAngular10Impl angular frontend

--- Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/home/home.component.ts
+++ Angular_8_JWT_Authentication_Authorization/BasicMEANCrudAngular10Impl/src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_service/user.service';
 
+/**
+ * This component class will user UserService to get the public content from backend
+ */
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  content: string;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getPublicContent().subscribe(
+      successfulResponse => {
+        this.content = successfulResponse;
+      }, error => {
+        this.content = JSON.parse(error.error).message;
+      }
+    );
   }
 
 }
